4ffc301ade2a35e5c2bebe385795e9d9f970c68b
kopalatommy <kopalatommy@gmail.com>
2019-11-25 16:01:42 -0700

    Fixed compile issue

--- M714/debug/moc_serialhandler.cpp
+++ M714/debug/moc_serialhandler.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_SerialHandler_t {
-    QByteArrayData data[13];
-    char stringdata0[135];
+    QByteArrayData data[12];
+    char stringdata0[122];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -41,15 +41,14 @@ QT_MOC_LITERAL(6, 68, 9), // "WriteSync"
 QT_MOC_LITERAL(7, 78, 4), // "data"
 QT_MOC_LITERAL(8, 83, 9), // "WriteChar"
 QT_MOC_LITERAL(9, 93, 1), // "c"
-QT_MOC_LITERAL(10, 95, 12), // "WriteMessage"
-QT_MOC_LITERAL(11, 108, 9), // "DataReady"
-QT_MOC_LITERAL(12, 118, 16) // "OnMessageTimeout"
+QT_MOC_LITERAL(10, 95, 16), // "WriteFullMessage"
+QT_MOC_LITERAL(11, 112, 9) // "DataReady"
 
     },
     "SerialHandler\0ReceivedNewData\0\0"
     "ReceivedError\0ReceivedAck\0ParsedData\0"
-    "WriteSync\0data\0WriteChar\0c\0WriteMessage\0"
-    "DataReady\0OnMessageTimeout"
+    "WriteSync\0data\0WriteChar\0c\0WriteFullMessage\0"
+    "DataReady"
 };
 #undef QT_MOC_LITERAL
 
@@ -59,7 +58,7 @@ static const uint qt_meta_data_SerialHandler[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       9,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -67,17 +66,16 @@ static const uint qt_meta_data_SerialHandler[] = {
        4,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,   59,    2, 0x06 /* Public */,
-       3,    0,   62,    2, 0x06 /* Public */,
-       4,    0,   63,    2, 0x06 /* Public */,
-       5,    0,   64,    2, 0x06 /* Public */,
+       1,    1,   54,    2, 0x06 /* Public */,
+       3,    0,   57,    2, 0x06 /* Public */,
+       4,    0,   58,    2, 0x06 /* Public */,
+       5,    0,   59,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       6,    1,   65,    2, 0x0a /* Public */,
-       8,    1,   68,    2, 0x0a /* Public */,
-      10,    1,   71,    2, 0x0a /* Public */,
-      11,    0,   74,    2, 0x08 /* Private */,
-      12,    0,   75,    2, 0x08 /* Private */,
+       6,    1,   60,    2, 0x0a /* Public */,
+       8,    1,   63,    2, 0x0a /* Public */,
+      10,    1,   66,    2, 0x0a /* Public */,
+      11,    0,   69,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void, QMetaType::QString,    2,
@@ -89,7 +87,6 @@ static const uint qt_meta_data_SerialHandler[] = {
     QMetaType::Void, QMetaType::QString,    7,
     QMetaType::Void, QMetaType::Char,    9,
     QMetaType::Void, QMetaType::QString,    7,
-    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -107,9 +104,8 @@ void SerialHandler::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _i
         case 3: _t->ParsedData(); break;
         case 4: _t->WriteSync((*reinterpret_cast< QString(*)>(_a[1]))); break;
         case 5: _t->WriteChar((*reinterpret_cast< char(*)>(_a[1]))); break;
-        case 6: _t->WriteMessage((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 6: _t->WriteFullMessage((*reinterpret_cast< QString(*)>(_a[1]))); break;
         case 7: _t->DataReady(); break;
-        case 8: _t->OnMessageTimeout(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -174,13 +170,13 @@ int SerialHandler::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 9)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 9;
+        _id -= 8;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 9)
+        if (_id < 8)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 9;
+        _id -= 8;
     }
     return _id;
 }
